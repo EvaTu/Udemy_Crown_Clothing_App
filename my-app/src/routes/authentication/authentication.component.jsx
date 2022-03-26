0064556049dabbc2e9cf6122ad949e5ef3c32267
EvaTu <evatuw@gmail.com>
2022-03-26 17:54:22 -0400

    firebase done

--- my-app/src/routes/authentication/authentication.component.jsx
+++ my-app/src/routes/authentication/authentication.component.jsx
@@ -1,11 +1,14 @@
 
-import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect} from "../../utils/firebase/firebase.utils"
+// import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect} from "../../utils/firebase/firebase.utils"
 // import {useEffect} from 'react'
 // import {getRedirectResult} from 'firebase/auth'
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component"
+import SignInForm from "../../components/sign-in-form/sign-in-form.component"
+import "./authentication.styles.scss"
 
-const SignIn = () => {
+
+const Authentication = () => {
 
 
     // useEffect(async () => {
@@ -16,10 +19,10 @@ const SignIn = () => {
     // }, [])
 
 
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopup()
-        const userDocRef = await createUserDocumentFromAuth(user)
-    }
+    // const logGoogleUser = async () => {
+    //     const {user} = await signInWithGooglePopup()
+    //     const userDocRef = await createUserDocumentFromAuth(user)
+    // }
 
     // const logGoogleRedirectUser = async () => {
     //     const {user} = await signInWithGoogleRedirect()
@@ -27,19 +30,22 @@ const SignIn = () => {
     // }
 
     return (
-        <div>
-            <h1>Sign In Page</h1>
-            <button onClick={logGoogleUser}>
+        <div className="authentication-container">
+            {/* <h1>Sign In Page</h1> */}
+            {/* <button onClick={logGoogleUser}>
                 Sign in with Goolge Popup
-            </button>
+            </button> */}
             {/* <button onClick={signInWithGoogleRedirect}>
                 Sign in with Goolge Redirect
             </button> */}
+            <SignInForm />
             <SignUpForm />
+            
+            
         </div>
     )
 
 }
 
 
-export default SignIn
+export default Authentication
