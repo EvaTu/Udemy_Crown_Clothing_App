a6126986199618c5a34fd454c45deed33859a0d9
EvaTu <evatuw@gmail.com>
2022-03-28 23:32:55 -0400

    completed most scss styles to styled-components

--- my-app/src/components/cart-icon/cart-icon.component.jsx
+++ my-app/src/components/cart-icon/cart-icon.component.jsx
@@ -1,5 +1,5 @@
-import "./cart-icon.styles.scss"
-import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
+import {CartIconContainer, ItemCount, ShoppingIcon} from "./cart-icon.styles.jsx"
+// import {ReactComponent as ShoppingIcon} from "../../assets/shopping-bag.svg"
 import {useContext} from 'react'
 import {CartContext} from "../../contexts/cart.context"
 
@@ -8,10 +8,10 @@ const CartIcon = () =>{
     const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen)
     // console.log(cartCount)
     return(
-        <div className="cart-icon-container" onClick={toggleIsCartOpen}>
+        <CartIconContainer onClick={toggleIsCartOpen}>
             <ShoppingIcon className="shopping-icon"/>
-            <span className="item-count">{cartCount}</span>
-        </div>
+            <ItemCount>{cartCount}</ItemCount>
+        </CartIconContainer>
     )
 }
 
